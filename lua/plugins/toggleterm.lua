8fb6d623c8aa8d966e7c8cc0627eee2436a7da3c
TJ Adams <teadams@protonmail.com>
2022-08-29 23:03:44 -0500

    Improve toggleterm movement and display
    This adds keymappings to control and toggle other terminals without
    first entering normal mode. It also adds line numbers to the terminal
    for easier navigation.

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -23,15 +23,27 @@ keymap("n", "<leader>t;", ":4 ToggleTerm<cr>", opts)
 
 keymap("n", "<leader>tt", ":ToggleTerm<cr>", opts)
 
-function _G.set_terminal_keymaps()
+-- Allow quick access to terminals in terminal mode as well
+keymap("t", "<leader>tj", "<C-\\><C-n>:1 ToggleTerm<cr>", opts)
+keymap("t", "<leader>tk", "<C-\\><C-n>:2 ToggleTerm<cr>", opts)
+keymap("t", "<leader>tl", "<C-\\><C-n>:3 ToggleTerm<cr>", opts)
+keymap("t", "<leader>t;", "<C-\\><C-n>:4 ToggleTerm<cr>", opts)
+
+keymap("t", "<leader>tt", "<C-\\><C-n>:ToggleTerm<cr>", opts)
+
+function _G.set_terminal_keymaps_options()
     local opts = {buffer = 0}
     vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
     vim.keymap.set("t", "<C-j>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateDown()<cr>", opts)
     vim.keymap.set("t", "<C-k>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateUp()<cr>", opts)
     vim.keymap.set("t", "<C-h>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateLeft()<cr>", opts)
     vim.keymap.set("t", "<C-l>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateRight()<cr>", opts)
+
+    vim.opt.number = true -- Show line numbers
+    vim.opt.relativenumber = true -- Show line numbers relative to current line
+
 end
 
 -- Have the aformentioned mappings only for toggleterm rather than terminal
 -- mode in general
-vim.cmd("autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()")
+vim.cmd("autocmd! TermEnter term://*toggleterm#* lua set_terminal_keymaps_options()")
