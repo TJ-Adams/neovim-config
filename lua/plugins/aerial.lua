c07f5ca553b874eaf3df7b339cb3f9616f4589fd
TJ Adams <teadams@protonmail.com>
2022-09-15 14:51:10 -0500

    Open aerial plugin view to the left
    This just forces the aerial plugin to open on the left side.

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -9,7 +9,7 @@ aerial.setup({})
 local keymap = vim.api.nvim_set_keymap
 local opts = {noremap = true, silent = true}
 
-keymap("n", "<leader>a", "<cmd>AerialToggle!<cr>", opts)
+keymap("n", "<leader>a", "<cmd>AerialToggle! left<cr>", opts)
 
 keymap("n", "{", "<cmd>AerialPrev<CR>", opts)
 keymap("n", "}", "<cmd>AerialNext<CR>", opts)
