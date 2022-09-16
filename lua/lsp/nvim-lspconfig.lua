b2909097ff7fe1e7e8706fb7d2576964b2ff130e
TJ Adams <teadams@protonmail.com>
2022-09-16 11:37:46 -0500

    Change lsp declaration keymap
    After having my configs for a while I think 'D' feels a bit more natural
    to me than 'c'.

--- lua/lsp/nvim-lspconfig.lua
+++ lua/lsp/nvim-lspconfig.lua
@@ -8,7 +8,7 @@ keymap('n', ']d', vim.diagnostic.goto_next, opts)
 -- Mappings.
 local bufopts = { noremap=true, silent=true, buffer=bufnr }
 
-vim.keymap.set('n', 'gc', vim.lsp.buf.declaration, bufopts)
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
 vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
 vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, bufopts)
