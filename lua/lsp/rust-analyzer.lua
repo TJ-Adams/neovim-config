f5ab509ca32e7da42cad5a445ba1121cd3c3494f
TJ Adams <teadams@protonmail.com>
2022-10-01 21:06:27 -0500

    Add rust lsp
    Adds basic lsp support for rust.

---
+++ lua/lsp/rust-analyzer.lua
@@ -0,0 +1,10 @@
+local status_ok, lspconfig = pcall(require, "lspconfig")
+if not status_ok then
+    return
+end
+
+lspconfig["rust_analyzer"].setup(
+    {
+        on_attach = require("aerial").on_attach
+    }
+)
