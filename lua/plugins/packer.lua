ee769d6c5184230216c1d86738b6e49ad49bd2fe
TJ Adams <teadams@protonmail.com>
2022-10-07 11:51:30 -0500

    Add mini.nvim
    mini.nvim is a plugin that provides a number of small features.
    Currently I'm just using mini.cursorword and mini.pairs.

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -130,6 +130,9 @@ return packer.startup(
         -- Improved highlighting for searching
         use 'kevinhwang91/nvim-hlslens'
 
+        -- Various small changes
+        use "echasnovski/mini.nvim"
+
         -- Automatically set up your configuration after cloning packer.nvim
         -- Put this at the end after all plugins
         if PACKER_BOOTSTRAP then
