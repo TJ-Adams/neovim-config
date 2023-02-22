bf27ded94911a63d0f440dc0b234de46d07a29dd
TJ Adams <teadams@protonmail.com>
2023-02-22 13:00:01 -0600

    Add smooth scrolling for 'gg' and 'G'

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -4,3 +4,10 @@ if not status_ok then
 end
 
 neoscroll.setup()
+
+local keymap = vim.api.nvim_set_keymap
+local opts = {noremap = true, silent = true}
+
+keymap("n", "gg", "<cmd>lua require'neoscroll'.scroll(-2*vim.api.nvim_buf_line_count(0), true, 1, 'circular')<cr>", opts)
+keymap("n", "G", "<cmd>lua require'neoscroll'.scroll(2*vim.api.nvim_buf_line_count(0), true, 1, 'circular')<cr>", opts)
+
