aef9840e5322b4a9a3c15f2204c605cab5707971
TJ Adams <teadams@protonmail.com>
2023-04-04 09:49:09 -0500

    lsp: clangd: Add note about removing unknown errors
    This adds a note about removing errors due to using compile flags that
    are known to gcc but unknown to clangd.

--- lua/lsp/clangd.lua
+++ lua/lsp/clangd.lua
@@ -2,3 +2,12 @@ require "lspconfig".clangd.setup(
     {
     }
 )
+
+-- NOTE: Some compile_commands.json will have gcc specific arguments
+-- that are unknown to clangd. For example -fno-diagnostics-show-caret
+-- is unknown to clangd. This will report the error in every file which
+-- can be annoying. In that case you can add a .clangd file to the root
+-- of your project with the following contents to ignore those.
+-- CompileFlags:
+--   Add: -Wno-unknown-warning-option
+--   Remove: [-m*, -f*]
