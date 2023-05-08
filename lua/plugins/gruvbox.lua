0c5e13d68dd61a4ca7915b094be8a804d7b89621
TJ Adams <teadams@protonmail.com>
2023-05-08 09:47:45 -0500

    Plugins: Gruvbox: Change TODO highlight colors
    I found the yellow highlights to be distracting, so I've changed the
    group to be more to what I like.

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -4,6 +4,7 @@
 
 local orange = "#FF6347"
 local black  = "#000000"
+local gruvbox_text  = "#EBDBB2"
 local grey  = "#303030"
 local dark_grey  = "#202020"
 
@@ -20,3 +21,6 @@ vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg=dark_grey })
 vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { bg=dark_grey })
 vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg=dark_grey })
 
+-- I think the yellow highlight is very distracting so I'm removing it
+vim.api.nvim_set_hl(0, "Todo", { fg = gruvbox_text, bold=true})
+vim.api.nvim_set_hl(0, "@text.todo", { fg=gruvbox_text, bold=true})
