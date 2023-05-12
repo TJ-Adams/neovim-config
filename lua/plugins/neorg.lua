b2df37b8a88779cda6448691f0cf52de6cab6e01
TJ Adams <teadams@protonmail.com>
2023-05-12 17:08:27 -0500

    Plugins: Neorg: Fix manoeuvre keybinds
    They were moved to core from core.norg.

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -33,8 +33,8 @@ neorg.setup(
                     config = {
                             hook = function(keybinds)
                                     -- Keybinds to make moving sections up and down easily
-                                    keybinds.map("norg", "n", "gj", "<cmd>Neorg keybind norg core.norg.manoeuvre.item_down<cr>")
-                                    keybinds.map("norg", "n", "gk", "<cmd>Neorg keybind norg core.norg.manoeuvre.item_up<cr>")
+                                    keybinds.map("norg", "n", "gj", "<cmd>Neorg keybind norg core.manoeuvre.item_down<cr>")
+                                    keybinds.map("norg", "n", "gk", "<cmd>Neorg keybind norg core.manoeuvre.item_up<cr>")
                                     keybinds.map("norg", "n", "]s", "<cmd>Neorg keybind norg core.integrations.treesitter.next.heading<cr>")
                                     keybinds.map("norg", "n", "[s", "<cmd>Neorg keybind norg core.integrations.treesitter.previous.heading<cr>")
                             end,
