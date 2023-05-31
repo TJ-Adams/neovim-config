3e271b07bdaed19c7c809b08a760c8d978bb2a21
TJ Adams <teadams@protonmail.com>
2023-05-31 09:11:13 -0500

    Plugins: nvim-tmux-navigation: Add terminal mode keymaps
    This change adds keymaps so that even if I'm in terminal mode I can
    still move around splits like usual.

--- lua/plugins/nvim-tmux-navigation.lua
+++ lua/plugins/nvim-tmux-navigation.lua
@@ -7,3 +7,7 @@ keymap("n", "<C-k>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateUp()<cr
 keymap("n", "<C-h>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateLeft()<cr>", opts)
 keymap("n", "<C-l>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateRight()<cr>", opts)
 
+keymap("t", "<C-j>", "<esc><cmd>lua require'nvim-tmux-navigation'.NvimTmuxNavigateDown()<cr>", opts)
+keymap("t", "<C-k>", "<esc><cmd>lua require'nvim-tmux-navigation'.NvimTmuxNavigateUp()<cr>", opts)
+keymap("t", "<C-h>", "<esc><cmd>lua require'nvim-tmux-navigation'.NvimTmuxNavigateLeft()<cr>", opts)
+keymap("t", "<C-l>", "<esc><cmd>lua require'nvim-tmux-navigation'.NvimTmuxNavigateRight()<cr>", opts)
