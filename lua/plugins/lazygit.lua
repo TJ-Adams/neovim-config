02907667b87fe0a076cc7b8f7214e1cedb0a6c0d
TJ Adams <teadams@protonmail.com>
2023-06-08 12:15:54 -0500

    Plugins: lazygit: Fix ctrl-j and ctrl-k in lazygit window
    The ctrl-j and ctrl-k keypresses which allow you to reorder commits was
    not working in the lazygit window because the tmux navigation commands
    were being called instead. This fixes that.

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -20,7 +20,13 @@ vim.cmd([[ autocmd BufEnter * :lua require('lazygit.utils').project_root_dir() ]
 -- Ensure lazygit recieves the escape key
 function _G.set_lazygit_keymaps()
     local lazygit_opts = {buffer = 0}
+
     vim.keymap.set("t", "<esc>", "<esc>", lazygit_opts)
+
+    vim.keymap.set("t", "<C-j>", "<C-j>", lazygit_opts)
+    vim.keymap.set("t", "<C-k>", "<C-k>", lazygit_opts)
+    vim.keymap.set("t", "<C-h>", "<C-h>", lazygit_opts)
+    vim.keymap.set("t", "<C-l>", "<C-l>", lazygit_opts)
 end
 
 vim.cmd("autocmd! TermEnter term://*lazygit* lua set_lazygit_keymaps()")
