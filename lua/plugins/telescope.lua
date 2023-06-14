ed08bc06bbba2722d399e992babe45d051e15f89
TJ Adams <teadams@protonmail.com>
2023-06-14 14:58:53 -0500

    Plugins: Telescope: Add recent files keymap
    This change adds a keymap to open recent files.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -59,6 +59,7 @@ keymap("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", opts)
 keymap("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", opts)
 keymap("n", "<leader>fu", "<cmd>lua require'telescope.builtin'.lsp_document_symbols({ symbols = 'function' })<cr>", opts) -- Fuzzy search functions
 keymap("n", "<leader>fm", "<cmd>Telescope treesitter<cr>i:macro:<esc>", opts) -- Fuzzy search macros
+keymap("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", opts) -- see recent files
 
 keymap("n", "<leader>fiw", "<cmd>Telescope grep_string<cr>", opts)
 keymap("n", "<leader>fib", "<cmd>Telescope current_buffer_fuzzy_find<cr>", opts)
