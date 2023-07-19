8d353cf05ab0d8b34c34936de77502864868103b
TJ Adams <teadams@protonmail.com>
2023-07-19 13:54:52 -0500

    Plugins: goto-preview: Add preview window specific keymaps
    This commit adds some keymaps that are triggered only while in a preview
    window. It also utilizes the post close hook feature that has recently
    been merged.

--- lua/plugins/goto-preview.lua
+++ lua/plugins/goto-preview.lua
@@ -4,10 +4,65 @@ if not status_ok then
     return
 end
 
-goto_preview.setup()
-
 local keymap = vim.keymap.set
 local opts = {noremap = true, silent = true}
+
+local function unset_keymaps()
+    keymap("n", "q", "q", opts)
+    keymap("n", "<C-j>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateDown()<cr>", opts)
+    keymap("n", "<C-k>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateUp()<cr>", opts)
+    keymap("n", "<C-h>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateLeft()<cr>", opts)
+    keymap("n", "<C-l>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateRight()<cr>", opts)
+end
+
+local function move_right_and_unset()
+    local key = vim.api.nvim_replace_termcodes("<C-w>L", true, false, true)
+    local curr_win = vim.api.nvim_get_current_win()
+
+    vim.api.nvim_feedkeys(key, "n", true)
+    vim.api.nvim_win_set_var(curr_win,"is-goto-preview-window", false)
+    unset_keymaps()
+end
+
+local function move_left_and_unset()
+    local key = vim.api.nvim_replace_termcodes("<C-w>H", true, false, true)
+    local curr_win = vim.api.nvim_get_current_win()
+
+    vim.api.nvim_feedkeys(key, "n", true)
+    vim.api.nvim_win_set_var(curr_win,"is-goto-preview-window", false)
+    unset_keymaps()
+end
+
+local function move_down_and_unset()
+    local key = vim.api.nvim_replace_termcodes("<C-w>J", true, false, true)
+    local curr_win = vim.api.nvim_get_current_win()
+
+    vim.api.nvim_feedkeys(key, "n", true)
+    vim.api.nvim_win_set_var(curr_win,"is-goto-preview-window", false)
+    unset_keymaps()
+end
+
+local function move_up_and_unset()
+    local key = vim.api.nvim_replace_termcodes("<C-w>K", true, false, true)
+    local curr_win = vim.api.nvim_get_current_win()
+
+    vim.api.nvim_feedkeys(key, "n", true)
+    vim.api.nvim_win_set_var(curr_win,"is-goto-preview-window", false)
+    unset_keymaps()
+end
+
+goto_preview.setup({
+    post_open_hook = function ()
+        keymap("n", "q", "<cmd>q<cr>", opts)
+        keymap("n", "<C-l>", function () move_right_and_unset() end, opts)
+        keymap("n", "<C-h>", function () move_left_and_unset() end, opts)
+        keymap("n", "<C-j>", function () move_down_and_unset() end, opts)
+        keymap("n", "<C-k>", function () move_up_and_unset() end, opts)
+    end,
+    post_close_hook = function ()
+        keymap("n", "q", "q", opts)
+    end,
+})
 
 keymap("n", "<leader>gd", "<cmd>lua require('goto-preview').goto_preview_definition()<cr>", opts)
 keymap("n", "<leader>gt", "<cmd>lua require('goto-preview').goto_preview_type_definition()<CR>", opts)
