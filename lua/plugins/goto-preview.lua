10372d76b76f890b29ff0feb8a67c587e201e182
TJ Adams <teadams@protonmail.com>
2023-08-03 14:32:17 -0500

    Plugins: goto-preview: Add close all keymap
    This adds a keymap for goto preview that closes all goto-preview
    windows. I use it periodically when I get into a weird bugged state
    where an old preview window is still visible but inaccessible.

--- lua/plugins/goto-preview.lua
+++ lua/plugins/goto-preview.lua
@@ -54,6 +54,7 @@ end
 goto_preview.setup({
     post_open_hook = function ()
         keymap("n", "q", "<cmd>q<cr>", opts)
+        keymap("n", "Q", "<cmd>lua require('goto-preview').close_all_win()<cr>", opts)
         keymap("n", "<C-l>", function () move_right_and_unset() end, opts)
         keymap("n", "<C-h>", function () move_left_and_unset() end, opts)
         keymap("n", "<C-j>", function () move_down_and_unset() end, opts)
@@ -61,6 +62,7 @@ goto_preview.setup({
     end,
     post_close_hook = function ()
         keymap("n", "q", "q", opts)
+        keymap("n", "Q", "<cmd>Q<cr>", opts)
         keymap("n", "<C-j>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateDown()<cr>", opts)
         keymap("n", "<C-k>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateUp()<cr>", opts)
         keymap("n", "<C-h>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateLeft()<cr>", opts)
