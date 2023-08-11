de3889a3ef0bbfa527980daab90fb07e84851985
TJ Adams <teadams@protonmail.com>
2023-08-11 10:16:27 -0500

    Plugins: lspconfig: Update keymap calls
    This is just a formatting change to make the file consistent.

--- lua/lsp/nvim-lspconfig.lua
+++ lua/lsp/nvim-lspconfig.lua
@@ -8,15 +8,15 @@ keymap("n", "]d", vim.diagnostic.goto_next, opts)
 -- Mappings.
 local bufopts = { noremap = true, silent = true, buffer = bufnr }
 
-vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+keymap("n", "gD", vim.lsp.buf.declaration, bufopts)
 
-vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+keymap("n", "K", vim.lsp.buf.hover, bufopts)
 
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
-vim.keymap.set("n", "<leader>bf", vim.lsp.buf.format, bufopts)
-vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
+keymap("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
+keymap("n", "<leader>bf", vim.lsp.buf.format, bufopts)
+keymap("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
 
-vim.keymap.set("v", "<leader>bf", vim.lsp.buf.format, bufopts)
+keymap("v", "<leader>bf", vim.lsp.buf.format, bufopts)
 
 local wk_status, wk = pcall(require, "which-key")
 if not wk_status then
