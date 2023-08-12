602fb24adf21a9286618b9b46c0e28d707181b0b
TJ Adams <teadams@protonmail.com>
2023-08-12 13:29:57 -0500

    Plugins: git-conflict.nvim: Add hydra submode
    Adds a hydra to handle git conflicts

--- lua/plugins/git-conflict.lua
+++ lua/plugins/git-conflict.lua
@@ -4,3 +4,41 @@ if not status_ok then
 end
 
 conflict.setup()
+
+local hydra_ok, hydra = pcall(require, "hydra")
+if not hydra_ok then
+    return
+end
+
+local hint = [[
+                   | Git Conflicts Mode |
+
+ _j_: next conflict   _o_: choose ours       _b_: choose both
+ _k_: prev conflict   _t_: choose theirs     _x_: choose none   _q_: quit
+]]
+
+local conflict_hydra = hydra({
+    name = "Git Conflicts",
+    hint = hint,
+    config = {
+        hint = {
+            border = "rounded",
+        },
+    },
+    heads = {
+        { "j", "<cmd>GitConflictNextConflict<cr>" },
+        { "k", "<cmd>GitConflictPrevConflict<cr>" },
+        { "o", "<cmd>GitConflictChooseOurs<cr>" },
+        { "t", "<cmd>GitConflictChooseTheirs<cr>" },
+        { "b", "<cmd>GitConflictChooseBoth<cr>" },
+        { "x", "<cmd>GitConflictChooseNone<cr>" },
+        { "q", nil, { exit = true } },
+    },
+})
+
+local keymap = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+keymap("n", "gc", function()
+    conflict_hydra:activate()
+end, opts)
