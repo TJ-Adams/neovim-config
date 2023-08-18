1a94e0ac0520c6695f64a77b3064066010a9683e
TJ Adams <teadams@protonmail.com>
2023-08-18 07:00:32 -0500

    Plugins: Add persistence.nvim
    This adds persistence.nvim. Taken from the lazyvim specifications.
    Eventually I'll most likely refactor to make it more aligned with the
    rest of the config.

--- lua/plugins/lazy-nvim.lua
+++ lua/plugins/lazy-nvim.lua
@@ -477,6 +477,19 @@ local plugins = {
     -- Surround plugin
     { "kylechui/nvim-surround", config = true },
 
+    -- Persistent Sessions
+    {
+        "folke/persistence.nvim",
+        event = "BufReadPre",
+        opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp" } },
+        -- stylua: ignore
+        keys = {
+            { "<leader>qs", function() require("persistence").load() end,                desc = "Restore Session" },
+            { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+            { "<leader>qd", function() require("persistence").stop() end,                desc = "Don't Save Current Session" },
+        },
+    },
+
     -- AI Plugins
     {
         "jackMort/ChatGPT.nvim",
