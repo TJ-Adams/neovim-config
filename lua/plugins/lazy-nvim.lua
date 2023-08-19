a3fbf484574ce30fab94462950b7f86c2f7b8a1e
TJ Adams <teadams@protonmail.com>
2023-08-18 21:38:46 -0500

    Plugins: alpha-nvim: Update buttons

--- lua/plugins/lazy-nvim.lua
+++ lua/plugins/lazy-nvim.lua
@@ -70,9 +70,10 @@ local plugins = {
             dashboard.section.header.val = vim.split(logo, "\n")
             dashboard.section.buttons.val = {
                 dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+                dashboard.button("p", " " .. " Open project", ":Telescope project<CR>"),
                 dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
                 dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
-                dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
+                dashboard.button("t", " " .. " Terminal", ":terminal<CR>"),
                 dashboard.button("s", " " .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
                 dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
                 dashboard.button("q", " " .. " Quit", ":q<CR>"),
