bd478381689010f63229f41f341f4512d79e5494
TJ Adams <teadams@protonmail.com>
2024-06-08 18:25:09 -0700

    Plugins: Telescope: Add file browser extension
    This adds an extension that shows a file browser in the Telescope
    window.

--- lua/plugins/lazy-nvim.lua
+++ lua/plugins/lazy-nvim.lua
@@ -156,6 +156,10 @@ local plugins = {
         },
         build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
     },
+    {
+        "nvim-telescope/telescope-file-browser.nvim",
+        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    },
 
     -- Project Based Notes
     {
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -74,6 +74,7 @@ telescope.load_extension "fzf"
 telescope.load_extension "project"
 telescope.load_extension "live_grep_args"
 telescope.load_extension "dap"
+telescope.load_extension "file_browser"
 
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
@@ -90,6 +91,7 @@ keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", opts)
 keymap("n", "<leader>fb", "<cmd>Telescope buffers<cr>", opts)
 keymap("n", "<leader>fl", "<cmd>Telescope resume<cr>", opts)
 keymap("n", "<leader>ft", "<cmd>Telescope<cr>", opts)
+keymap("n", "<leader>fe", "<cmd>Telescope file_browser path=%:p:h select_buffer=true<CR>", opts)
 keymap("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", opts)
 keymap("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", opts)
 keymap(
