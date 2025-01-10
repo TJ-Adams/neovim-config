be6cb9f6fd71d0085b426be196955dd0d76fa079
TJ Adams <teadams@protonmail.com>
2025-01-09 19:52:51 -0800

    Auto create parent directories when writing
    Taken directly from the documentation here:
    
    https://neovim.io/doc/user/editing.html

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,3 +1,6 @@
 -- Open up help documents in a vertical split instead
 -- of horizontal
 vim.cmd [[ autocmd! FileType help :wincmd L ]]
+
+-- Auto-create parent directories (except for URIs "://").
+vim.cmd [[ au BufWritePre,FileWritePre * if @% !~# '\(://\)' | call mkdir(expand('<afile>:p:h'), 'p') | endif ]]
