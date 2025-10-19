c7bd379aa0d44873766086144875f0415e3dc404
TJ Adams <teadams@protonmail.com>
2025-10-19 16:34:52 -0700

    plugins: flash: Have flash work on search

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -3,7 +3,24 @@ if not status_ok then
     return
 end
 
-flash.setup()
+flash.setup({
+    label = {
+        -- Don't allow uppercase labels
+        uppercase = false,
+    },
+    modes = {
+        -- options used when flash is activated through
+        -- a regular search with `/` or `?`
+        search = {
+            -- when `true`, flash will be activated during regular search by default.
+            -- You can always toggle when searching with `require("flash").toggle()`
+            enabled = true,
+            search = {
+                multi_window = false,
+            },
+        },
+    },
+})
 
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
