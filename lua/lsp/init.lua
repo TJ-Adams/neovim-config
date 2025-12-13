cad7ec4b77fb2dbd15a83b15b2e45c8546efaa2b
TJ Adams <teadams@protonmail.com>
2025-12-13 08:41:50 -0800

    plugins: Replace nvim-cmp with blink.cmp

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,6 +1,5 @@
 require "lsp/nvim-lspconfig"
 require "lsp/mason"
-require "lsp/nvim-cmp"
 require "lsp/lsp_signature"
 
 require "lsp/pyright"
---
+++ lua/plugins/blink.lua
@@ -0,0 +1,34 @@
+local blink = require "blink.cmp"
+local opts = {
+    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+    -- 'super-tab' for mappings similar to vscode (tab to accept)
+    -- 'enter' for enter to accept
+    -- 'none' for no mappings
+    --
+    -- All presets have the following mappings:
+    -- C-space: Open menu or open docs if already open
+    -- C-n/C-p or Up/Down: Select next/previous item
+    -- C-e: Hide menu
+    -- C-k: Toggle signature help (if signature.enabled = true)
+    --
+    -- See :h blink-cmp-config-keymap for defining your own keymap
+    keymap = {
+        preset = "enter",
+        ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-d>"] = { "scroll_documentation_down", "fallback" },
+    },
+
+    -- (Default) Only show the documentation popup when manually triggered
+    completion = { documentation = { auto_show = true }, list = { selection = { preselect = false } } },
+
+    -- Default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, due to `opts_extend`
+    sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+        per_filetype = {
+            norg = { "path" },
+        },
+    },
+}
+
+blink.setup(opts)
