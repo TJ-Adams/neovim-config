c2c8c680e99fa587d2c2a47a78c3105f676941d0
TJ Adams <teadams@protonmail.com>
2025-12-18 07:07:54 -0800

    Change Explorer to snacks.explorer

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,3 +1,5 @@
+vim.keymap.set("n", "<leader>e", function() require("snacks").explorer.reveal() end)
+
 return {
     "folke/snacks.nvim",
     opts = {
@@ -5,6 +7,19 @@ return {
         styles = {
             input = {
                 relative = "cursor",
+            },
+        },
+        explorer = {
+            -- your explorer configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+        picker = {
+            sources = {
+                explorer = {
+                    -- your explorer picker configuration comes here
+                    -- or leave it empty to use the default settings
+                },
             },
         },
     },
