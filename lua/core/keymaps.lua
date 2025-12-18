c72344ccd44966acc4b327ef8fbbf2d958692f79
TJ Adams <teadams@protonmail.com>
2025-12-17 21:52:55 -0800

    Add keymap for toggling virtual lines

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -74,10 +74,13 @@ end, { expr = true })
 keymap("n", "gD", vim.lsp.buf.declaration, {desc = "Go To Declarations", silent = true })
 keymap("n", "K", vim.lsp.buf.hover, {desc = "Info Under Cursor", silent = true })
 
-keymap("n", "<leader>do", vim.diagnostic.open_float, {desc = "Show Diagnostics", silent = true })
-keymap("n", "<leader>ca", vim.lsp.buf.code_action, {desc = "Code Action", silent = true })
-keymap("n", "<leader>bf", vim.lsp.buf.format, {desc = "Format Buffer", silent = true })
-keymap("n", "<leader>rn", vim.lsp.buf.rename, {desc = "Rename", silent = true })
+keymap("n", "<leader>do", function()
+    local new_config = not vim.diagnostic.config().virtual_lines
+    vim.diagnostic.config({ virtual_lines = new_config })
+end, { desc = "Toggle diagnostic virtual_lines" })
+keymap("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action", silent = true })
+keymap("n", "<leader>bf", vim.lsp.buf.format, { desc = "Format Buffer", silent = true })
+keymap("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename", silent = true })
 
 keymap("n", "<leader>h", function()
     vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
