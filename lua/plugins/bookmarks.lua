92ceb5a86286fce0e847348a3bde5ab12f35a6d2
TJ Adams <teadams@protonmail.com>
2025-12-24 13:46:30 -0800

    bookmarks: Only set desired keymaps

--- lua/plugins/bookmarks.lua
+++ lua/plugins/bookmarks.lua
@@ -1,9 +1,19 @@
 vim.keymap.set("n", "mg", "<cmd>Telescope bookmarks<cr>", { desc = "Go to Bookmark", silent = true })
 
+vim.keymap.set("n", "ma", function()
+    require("bookmarks").add_bookmarks(false)
+end)
+vim.keymap.set("n", "md", function()
+    require("bookmarks.list").delete_on_virt()
+end)
+vim.keymap.set("n", "ms", function()
+    require("bookmarks.list").show_desc()
+end)
+
 return {
     "crusj/bookmarks.nvim",
     opts = {
-        mappings_enabled = true,
+        mappings_enabled = false,
         keymap = {
             add = "ma",
             delete_on_virt = "md", -- Delete Mark
