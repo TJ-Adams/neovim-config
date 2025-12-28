88a5f660e8a942b40e413ce5fd7b6015ff16e424
TJ Adams <teadams@protonmail.com>
2025-12-28 11:29:37 -0800

    Remove Mason

--- lua/plugins/mason.lua
+++
@@ -1,6 +0,0 @@
-return {
-    { "williamboman/mason.nvim", opts = {}, build = ":MasonUpdate" },
-    { "williamboman/mason-lspconfig.nvim", dependencies = {
-        "williamboman/mason.nvim",
-    } },
-}
