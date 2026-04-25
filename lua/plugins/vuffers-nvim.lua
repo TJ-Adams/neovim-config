1a224f729726cb9bf7988419f4f09a0e84ccdc63
TJ Adams <teadams@protonmail.com>
2026-04-25 14:05:47 -0700

    plugins: replace bufferline -> vuffers

---
+++ lua/plugins/vuffers-nvim.lua
@@ -0,0 +1,40 @@
+return {
+    "Hajime-Suzuki/vuffers.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+        exclude = {
+            filenames = {},
+        },
+        keymaps = {
+            view = {
+                move_up = "<leader>bk",
+                move_down = "<leader>bj",
+            },
+        },
+        view = {
+            show_file_extension = true,
+        },
+    },
+    keys = {
+        {
+            "<leader>bl",
+            function()
+                require("vuffers").toggle()
+            end,
+        },
+        {
+            "<S-h>",
+            function()
+                require("vuffers").go_to_buffer_by_count({ direction = "prev", count = 1 })
+            end,
+        },
+        {
+            "<S-l>",
+            function()
+                require("vuffers").go_to_buffer_by_count({ direction = "next", count = 1 })
+            end,
+        },
+    },
+}
+
+
