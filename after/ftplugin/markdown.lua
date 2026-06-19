71b9395df0c35adf63a46db31d894f10e7c71ce4
TJ Adams <teadams@protonmail.com>
2026-06-19 12:59:05 -0700

    markdown: Add a buffer formatting keymap

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,1 +1,6 @@
 vim.opt.conceallevel = 0
+
+-- Format Markdown Files
+vim.keymap.set("n", "<leader>bf", function()
+    vim.cmd "%!mdformat --wrap 100 -"
+end, { buffer = true, desc = "Format Buffer (mdformat)", silent = true })
