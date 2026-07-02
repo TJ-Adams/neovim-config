9204badc189848930354530d85fd3f1a5dcc4e4c
TJ Adams <teadams@protonmail.com>
2026-07-02 13:49:05 -0700

    markdown: preserve numbering when formatting

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -2,5 +2,7 @@ vim.opt.conceallevel = 0
 
 -- Format Markdown Files
 vim.keymap.set("n", "<leader>bf", function()
-    vim.cmd "%!mdformat --wrap 100 -"
+    -- `--number` keeps consecutive 1. 2. 3. numbering; without it mdformat
+    -- collapses every ordered-list item to `1.`.
+    vim.cmd "%!mdformat --number --wrap 100 -"
 end, { buffer = true, desc = "Format Buffer (mdformat)", silent = true })
